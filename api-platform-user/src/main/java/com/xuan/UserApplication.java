c03d3962c7bb48d74057ef938e12a12f879806c0
xuan <zx786@vip.qq.com>
2023-05-05 15:31:35 +0800

    change project name

--- api-platform-user/src/main/java/com/xuan/UserApplication.java
+++ api-platform-user/src/main/java/com/xuan/UserApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author: xuan
+ * @since: 2023/3/28
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.xuan.mapper")
 public class UserApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
 
 }
